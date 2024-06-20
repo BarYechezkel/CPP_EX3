03e7c072cd233683c0cfd80df7c897208d33579e
BarYechezkel <baryechezkel@gmail.com>
2024-06-20 20:06:10 +0300

    update

--- Catan.cpp
+++ Catan.cpp
@@ -16,6 +16,54 @@ class Board;
 // cout << "🌾 Wheat: " << resources[4] << endl;  // Wheat emoji
 // cout << "🪨 Iron:
 // 🌲🧱 🐑🌾🪨
+int main()
+{
+    Player p1("Player1");
+    Player p2("Player2");
+    Player p3("Player3");
+    Board board;
+    Catan catan(p1, p2, p3, board);
+    // p2.addPoints(10);
+    catan.StartGame();
+    catan.progressGame();
+    // p1.setColor(2);
+    // p1.placeSettlementFirst(board,8);
+    // p1.placeRoad(board,12);
+    // p1.placeRoad(board,11);
+    // p1.placeSettlement(board,7);
+    // p1.placeRoad(board,10);
+    // board.printBoard();
+
+    // cout << "enter cin"<<endl;
+    // int x;
+    // cin >> x;
+    // while (p1.placeSettlement(board, x) == 0)
+    // {
+    //     cout << "enter cin"<<endl;
+    //     cin >> x;
+    // }
+
+    // board.printBoard();
+    //    board.printBoard();
+    //  //  p1.placeSettlement(board, 0);
+    //    p1.placeRoad(board, 0);
+    //     p1.placeCity(board, 0);
+    //    p1.printResources();
+    //  board.printBoard();
+
+    //     p2.placeSettlement(board, 3);
+    //     p1.placeSettlement(board, 4);
+    //     p3.placeSettlement(board, 7);
+    //     p2.placeSettlement(board, 4);
+    //     board.getVertex(3)->printVertex();
+    //     p2.placeRoad(board, 0);
+    //     p1.placeRoad(board, 4);
+    //     p2.placeCity(board, 3);
+
+    //    board.printBoard();
+    // catan.getBoard().printBoard();
+    return 0;
+}
 
 void Catan::SetPlayerColor()
 {
--- makefile
+++ makefile
@@ -1,38 +1,15 @@
-# # Define the C++ compiler to use
-# CC = g++
-
-# # Define any compile-time flags
-# CFLAGS = -Wall -g
-
-# # Define the target file (the executable)
-# TARGET = Catan
-
-# # Define the C++ source files
-# SRCS = Catan.cpp Vertex.cpp Edge.cpp Tile.cpp Player.cpp Board.cpp
-
-# all: $(TARGET)
-
-# $(TARGET): $(SRCS)
-# 	$(CC) $(CFLAGS) -o $(TARGET) $(SRCS)
-
-# clean:
-# 	$(RM) $(TARGET)
-
 
 
 CXX = clang++
 
 .PHONY: all clean
 
-all: main
+all: Catan
 
-main: main.o Catan.o Tile.o Player.o Board.o Vertex.o Edge.o Knight.o RoadBuilding.o Monopoly.o YearOfPlenty.o VictoryPoint.o
+Catan:  Catan.o Tile.o Player.o Board.o Vertex.o Edge.o Knight.o RoadBuilding.o Monopoly.o YearOfPlenty.o VictoryPoint.o
 	$(CXX) -o $@ $^
 
-main.o: main.cpp Catan.hpp
-	$(CXX) -c -o $@ $<
-
-Catan.o: Catan.cpp 
+Catan.o: Catan.cpp Catan.hpp
 	$(CXX) -c -o $@ $<
 	
 Player.o: Player.cpp Player.hpp
@@ -49,9 +26,6 @@ Vertex.o: Vertex.cpp Vertex.hpp
 
 Edge.o: Edge.cpp Edge.hpp
 	$(CXX) -c -o $@ $<
-
-# DevCard.o: DevCard.cpp DevCard.hpp
-# 	$(CXX) -c -o $@ $<
 
 Knight.o: Knight.cpp Knight.hpp
 	$(CXX) -c -o $@ $<
@@ -70,4 +44,4 @@ VictoryPoint.o: VictoryPoint.cpp VictoryPoint.hpp
 
 
 clean:
-	 rm -f main *.o
+	 rm -f Catan *.o
